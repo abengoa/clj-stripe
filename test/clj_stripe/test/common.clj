bf030d8bc7ce0d63018ed7a9d9130933eaa03c72
Alberto Bengoa <abengoam@gmail.com>
2011-10-03 23:27:57 -0700

    Tests for common namespace.

--- test/clj_stripe/test/common.clj
+++ test/clj_stripe/test/common.clj
@@ -3,10 +3,41 @@
   (:require [clojure.test :as test]))
 
 
-
-(def test-address (address (address1 "address line 1") (address2 "address line 2") (zip 90123) (state "ca") (country "us")))
-
+(def sample-address (address (address1 "address line 1") (address2 "address line 2") (zip 90123) (state "ca") (country "us")))
 
 (test/deftest address-fn-test
-  (test/is (= test-address {"card[address_line_1]" "address line 1", "card[address_line_2]" "address line 2", "card[address_zip]" 90123, "card[address_state]" "ca", "card[address_country]" "us"}))
-)
+  (test/is (= 
+    sample-address
+    {"card[address_line_1]" "address line 1", "card[address_line_2]" "address line 2", "card[address_zip]" 90123, "card[address_state]" "ca", "card[address_country]" "us"}))
+  (test/is (= 
+    (address (address2 "address line 2") (zip 90123))
+    {"card[address_line_2]" "address line 2", "card[address_zip]" 90123}))
+  )
+
+(test/deftest expiration-date-test
+  (test/is (= (expiration 5 2011) {"card[exp_month]" 5, "card[exp_year]" 2011}))
+  )
+
+(test/deftest card-test
+  (test/is (= (card (number "4242424242424242") (expiration 12 2012)) {
+	      "card[number]" "4242424242424242", "card[exp_month]" 12, "card[exp_year]" 2012}))
+  (test/is (= (card (number "4242424242424242") (expiration 12 2012) (cvc 123) (description "a normal cc") (elem-name "my first cc"))
+	      {"card[number]" "4242424242424242", "card[exp_month]" 12, "card[exp_year]" 2012, "card[cvc]" 123, "description" "a normal cc", "name" "my first cc"}))
+  )
+
+(test/deftest position-test
+  (test/is (= (offset 5) {"offset" 5}))
+  (test/is (= (limit-count 6) {"count" 6}))
+  (test/is (= (position 6 5) {"offset" 5 "count" 6}))
+  )
+
+(test/deftest plan-test
+  (test/is (= (coupon "mycoupon") {"coupon" "mycoupon"}))
+  (test/is (= (plan "myplan") {"plan" "myplan"}))
+  (test/is (= (trial-end 12345) {"trial_end" 12345}))
+  )
+
+(test/deftest money
+  (test/is (= (amount 3000) {"amount" 3000}))
+  (test/is (= (money-quantity 3000 "usd") {"amount" 3000, "currency" "usd"}))
+  )
