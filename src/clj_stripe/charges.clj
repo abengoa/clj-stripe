295b924ae971fe4d0806f6dd0529352344b05442
Alberto Bengoa <abengoam@gmail.com>
2011-10-04 19:08:35 -0700

    Commented the charges API.

--- src/clj_stripe/charges.clj
+++ src/clj_stripe/charges.clj
@@ -4,28 +4,43 @@
     (:require [clj-stripe.util :as util]))
 
 (defn customer
-	[customer-id]
-	{"customer" customer-id})
+   "Creates the data for a customer identifier"
+  [customer-id]
+  {"customer" customer-id})
 
 (defn create-charge
+  "Creates a new charge operation.
+  Requires one money quantity (see common/money-quantity), and either a customer (see customer) or a card (see common/card).
+  Optionally accepts a desciption (see common/description).
+  Execute the operation using common/execute."
   [money-quantity & extra-info]
-  (apply util/merge-maps {:operation :charge} money-quantity extra-info )
-  )
+  (apply util/merge-maps {:operation :charge} money-quantity extra-info))
+
 (defmethod execute :charge [op-data] (util/post-request stripe-token (str *api-root* "/charges") (dissoc op-data :operation)))
 
 (defn get-charge
+  "Creates a new get-charge operation.
+  Requires a charge id as a string.
+  Execute the operation using common/execute."
   [charge-id]
   {:operation :get-charge "id" charge-id}
   )
 (defmethod execute :get-charge [op-data] (util/get-request stripe-token (str *api-root* "/charges/" (get op-data "id"))))
 
 (defn get-all-charges
+  "Creates a new get-all-charges operation.
+  Optionally accepts a customer (see common/customer), offset and count (see common/offset, common/limit-count and common/position).
+  Execute the operation using common/execute."
   ([& extra-info] 
     (apply util/merge-maps {:operation :get-all-charges} extra-info))
   )
 (defmethod execute :get-all-charges [op-data] (util/get-request stripe-token (util/url-with-optional-params (str *api-root* "/charges") op-data ["customer" "count" "offset"])))
 
 (defn create-refund
+  "Creates a charge-refund operation.
+  Requires the charge id (as a string).
+  Optionally accepts the amount to be refunded (see common/amount).
+  Execute the operation using common/execute."
   ([charge-id] (create-refund charge-id nil))
   ([charge-id amount] (util/merge-maps {:operation :refund-charge "id" charge-id } amount))
   )
