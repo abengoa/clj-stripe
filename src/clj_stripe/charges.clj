327ec10822df75f482bbb2d530176b0b35a20591
Alberto Bengoa <abengoam@gmail.com>
2011-10-04 19:10:18 -0700

    Pretified the charges API.

--- src/clj_stripe/charges.clj
+++ src/clj_stripe/charges.clj
@@ -16,25 +16,31 @@
   [money-quantity & extra-info]
   (apply util/merge-maps {:operation :charge} money-quantity extra-info))
 
-(defmethod execute :charge [op-data] (util/post-request stripe-token (str *api-root* "/charges") (dissoc op-data :operation)))
+(defmethod execute :charge 
+  [op-data] 
+  (util/post-request stripe-token (str *api-root* "/charges") (dissoc op-data :operation)))
 
 (defn get-charge
   "Creates a new get-charge operation.
   Requires a charge id as a string.
   Execute the operation using common/execute."
   [charge-id]
-  {:operation :get-charge "id" charge-id}
-  )
-(defmethod execute :get-charge [op-data] (util/get-request stripe-token (str *api-root* "/charges/" (get op-data "id"))))
+  {:operation :get-charge "id" charge-id})
+
+(defmethod execute :get-charge 
+  [op-data] 
+  (util/get-request stripe-token (str *api-root* "/charges/" (get op-data "id"))))
 
 (defn get-all-charges
   "Creates a new get-all-charges operation.
   Optionally accepts a customer (see common/customer), offset and count (see common/offset, common/limit-count and common/position).
   Execute the operation using common/execute."
   ([& extra-info] 
-    (apply util/merge-maps {:operation :get-all-charges} extra-info))
-  )
-(defmethod execute :get-all-charges [op-data] (util/get-request stripe-token (util/url-with-optional-params (str *api-root* "/charges") op-data ["customer" "count" "offset"])))
+    (apply util/merge-maps {:operation :get-all-charges} extra-info)))
+
+(defmethod execute :get-all-charges 
+  [op-data] 
+  (util/get-request stripe-token (util/url-with-optional-params (str *api-root* "/charges") op-data ["customer" "count" "offset"])))
 
 (defn create-refund
   "Creates a charge-refund operation.
@@ -42,7 +48,9 @@
   Optionally accepts the amount to be refunded (see common/amount).
   Execute the operation using common/execute."
   ([charge-id] (create-refund charge-id nil))
-  ([charge-id amount] (util/merge-maps {:operation :refund-charge "id" charge-id } amount))
-  )
-(defmethod execute :refund-charge [op-data] (util/post-request stripe-token (str *api-root* "/charges/" (get op-data "id") "/refund") (dissoc op-data :operation)))
+  ([charge-id amount] (util/merge-maps {:operation :refund-charge "id" charge-id } amount)))
+
+(defmethod execute :refund-charge 
+  [op-data]
+  (util/post-request stripe-token (str *api-root* "/charges/" (get op-data "id") "/refund") (dissoc op-data :operation)))
 
