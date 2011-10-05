ee44c2b195eb1d6b85ba383b0d06f2255513ca7c
Alberto Bengoa <abengoam@gmail.com>
2011-10-04 20:05:31 -0700

    Tests for customers namespace.

---
+++ test/clj_stripe/test/customers.clj
@@ -0,0 +1,43 @@
+(ns clj-stripe.test.customers
+  (:use [clj-stripe common customers])
+  (:require [clojure.test :as test]))
+
+(with-token "vtUQeOtUnYr7PGCLQ96Ul4zqpDUO4sOE:"
+
+  (def test-card (card (number "4242424242424242") 
+		        (expiration 12 2012)
+			(cvc 123)
+			(description "a normal cc")
+			(elem-name "my first cc")))
+  (def create-customer-op (create-customer 
+			      test-card
+			      (email "mrtest@teststripe.com")
+			      (description "A test customer") 
+			      (trial-end (+ 10000 (System/currentTimeMillis)))))
+
+  (def customer-result (execute create-customer-op))
+  (def get-customer-op (get-customer (:id customer-result)))
+  (def get-customer-result (execute get-customer-op))
+  (def get-all-customers-op (get-customers))
+  (def get-all-customers-result (execute get-all-customers-op))
+
+  (test/deftest customers-test
+    (test/is (= get-customer-result customer-result))
+    (test/is (some #{(:id get-customer-result)} (map :id (:data get-all-customers-result))))
+    )
+
+  (def new-email (email "a new email"))
+  (def update-customer-op (update-customer (:id customer-result) new-email))
+  (def update-customer-result (execute update-customer-op))
+  (def get-customer-op-2 (get-customer (:id customer-result)))
+  (def get-customer-result-2 (execute get-customer-op-2))
+
+  (test/deftest modify-customer-test
+    (test/is (= (assoc customer-result :email (get new-email "email")) get-customer-result-2)))
+  
+  (def delete-customer-op (delete-customer (:id customer-result)))
+  (def delete-customer-result (execute delete-customer-op))
+  (def get-all-customers-result-2 (execute get-all-customers-op))
+
+  (test/deftest delete-customer-test
+    (test/is (not (nil? (some #{(:id get-customer-result)} (map :id (:data get-all-customers-result-2))))))))
