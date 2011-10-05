54b2d5853df007fc14bbb0b5dadcb08b95b820e5
Alberto Bengoa <abengoam@gmail.com>
2011-10-04 22:29:29 -0700

    Gitignore.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clj-stripe "1.0.0-SNAPSHOT"
+  :description "Clojure binding for Stripe"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+		 [clojure-contrib "1.2.0"]
+		 [clj-http "0.2.1"]])
