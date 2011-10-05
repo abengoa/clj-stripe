5205fc292c7453cd0808594cf9e1de6610d1cf16
Alberto Bengoa <abengoam@gmail.com>
2011-10-04 20:52:42 -0700

    Removed old files.

--- src/clj_stripe/core.clj
+++
@@ -1,1 +0,0 @@
-(ns clj-stripe.core)
--- test/clj_stripe/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns clj-stripe.test.core
-  (:use [clj-stripe.core])
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
