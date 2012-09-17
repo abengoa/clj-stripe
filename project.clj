7d342a9beac4f1dd896d192974bdb2f13603a8bc
siscia <simone@mweb.biz>
2012-09-17 18:50:59 +0200

    make it work with clj 1.3

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clj-stripe "1.0.0-SNAPSHOT"
   :description "Clojure binding for Stripe"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-		 [clojure-contrib "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+		 [org.clojure/data.json "0.1.3"]
 		 [clj-http "0.2.1"]])
