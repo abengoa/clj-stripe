23af7d75497c681726724abd75eaf506dbc7dfdd
abengoa <abengoam@gmail.com>
2012-09-23 21:03:09 -0700

    Update project.clj
    Updated version from 1.0.0 to 1.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject abengoa/clj-stripe "1.0.0"
+(defproject abengoa/clj-stripe "1.0.1"
   :description "Clojure binding for Stripe"
   :dependencies [[org.clojure/clojure "1.2.1"]
 		 [clojure-contrib "1.2.0"]
