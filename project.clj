5f550f332898f4bec9228b7bf8cf55940a4eecb1
Frozenlock <frozenlock@gmail.com>
2013-03-06 18:11:06 -0500

    update clojure and clj-http versions

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject abengoa/clj-stripe "1.0.3"
+(defproject org.clojars.frozenlock/clj-stripe "1.0.3"
   :description "Clojure binding for Stripe"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
 		 [org.clojure/data.json "0.1.3"]
-		 [clj-http "0.2.1"]])
+		 [clj-http "0.6.5"]])
