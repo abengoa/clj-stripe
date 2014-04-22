ba91e53143ce5bcac9697256778702196e656426
Prachetas Prabhu <pprabhu@itemizecorp.com>
2014-04-21 22:52:58 -0400

    Removes data.json dependency

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject abengoa/clj-stripe "1.0.4"
   :description "Clojure binding for Stripe"
   :dependencies [[org.clojure/clojure "1.5.1"]
-		 [org.clojure/data.json "0.2.4"]
 		 [clj-http "0.9.1"]])
